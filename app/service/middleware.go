931fd2bbc382f44dca778853aeedd62d029f7091
mutun <tun224813@gmail.com>
2021-04-28 17:14:54 +0800

    添加:中间件相关方法

---
+++ app/service/middleware.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"gf-L/app/model"
+	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/util/gconv"
+)
+
+// 中间件服务
+var Middleware = middlewareStruct{
+	loginUrl: "/login",
+}
+
+// 获取用户登录URL
+func (s *middlewareStruct) GetLoginUrl() string {
+	return s.loginUrl
+}
+
+type middlewareStruct struct {
+	loginUrl string
+}
+
+// 初始化上下文 存在用户则放入上下文
+func (middlewareStruct) Ctx(r *ghttp.Request) {
+	session := r.Session
+	ctx := &model.Context{
+		Session: session,
+		User:    nil,
+		Data:    nil,
+	}
+	Context.Init(r, ctx)
+	if session != nil {
+		user := Session.GetUser(r.Context())
+		if user != nil {
+			var contextUser *model.ContextUser
+			gconv.Struct(user, contextUser)
+			Context.SetUser(r.Context(), contextUser)
+		}
+	}
+
+	r.Middleware.Next()
+}
